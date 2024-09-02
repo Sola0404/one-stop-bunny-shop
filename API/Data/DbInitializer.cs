be3fdf438f5a52eb30cc3b876854b7cb5b2bd410
Yujia <0404sola@gmail.com>
2024-09-02 17:04:12 +1000

    Seed product data

---
+++ API/Data/DbInitializer.cs
@@ -0,0 +1,219 @@
+using API.Entities;
+
+namespace API.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(StoreContext context)
+        {
+            if (context.Products.Any()) return;
+
+            var products = new List<Product>
+            {
+                new Product
+                {
+                    Name = "Senior Rabbit Pellets",
+                    Description =
+                        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 20000,
+                    PictureUrl = "/images/products/senior-rabbit-pellets.png",
+                    Brand = "Oxbow",
+                    Type = "Pellets",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Young Rabbit Pellets",
+                    Description = "Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus.",
+                    Price = 15000,
+                    PictureUrl = "/images/products/young-rabbit-pellets.png",
+                    Brand = "Oxbow",
+                    Type = "Pellets",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Adult Rabbit Pellets",
+                    Description =
+                        "Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+                    Price = 18000,
+                    PictureUrl = "/images/products/adult-rabbit-pellets.png",
+                    Brand = "Oxbow",
+                    Type = "Pellets",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Junior Rabbit Food",
+                    Description =
+                        "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.",
+                    Price = 30000,
+                    PictureUrl = "/images/products/junior-rabbit-food.png",
+                    Brand = "Science Selective",
+                    Type = "Pellets",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Adult Rabbit Food",
+                    Description =
+                        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 25000,
+                    PictureUrl = "/images/products/adult-rabbit-food.png",
+                    Brand = "Science Selective",
+                    Type = "Pellets",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Oaten Hay",
+                    Description =
+                        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 12000,
+                    PictureUrl = "/images/products/oaten-hay.png",
+                    Brand = "Self-Grown",
+                    Type = "Hay",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Teff Hay",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 1000,
+                    PictureUrl = "/images/products/teff-hay.png",
+                    Brand = "Self-Grown",
+                    Type = "Hay",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Lucerne Hay",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 8000,
+                    PictureUrl = "/images/products/lucerne-hay.png",
+                    Brand = "Self-Grown",
+                    Type = "Hay",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Timothy Hay",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 1500,
+                    PictureUrl = "/images/products/timothy-hay.png",
+                    Brand = "Oxbow",
+                    Type = "Hay",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Orchard Grass Hay",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 1800,
+                    PictureUrl = "/images/products/orchard-grass-hay.png",
+                    Brand = "Oxbow",
+                    Type = "Hay",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Joint Support Cookies",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 1500,
+                    PictureUrl = "/images/products/joint-support.png",
+                    Brand = "Oxbow",
+                    Type = "Treats",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Urinary Support Cookies",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 1600,
+                    PictureUrl = "/images/products/urinary-support.png",
+                    Brand = "Oxbow",
+                    Type = "Treats",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Vitamin C Cookies",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 1400,
+                    PictureUrl = "/images/products/vitamin-c.png",
+                    Brand = "Oxbow",
+                    Type = "Treats",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Digestive Support Cookies",
+                    Description =
+                        "Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+                    Price = 25000,
+                    PictureUrl = "/images/products/digestive-support.png",
+                    Brand = "Oxbow",
+                    Type = "Treats",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Slicker Brush",
+                    Description =
+                        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 18999,
+                    PictureUrl = "/images/products/brush.png",
+                    Brand = "Others",
+                    Type = "Accessories",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Nail Clipper",
+                    Description =
+                        "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.",
+                    Price = 19999,
+                    PictureUrl = "/images/products/nail-clipper.png",
+                    Brand = "Others",
+                    Type = "Accessories",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Water Bottle",
+                    Description = "Aenean nec lorem. In porttitor. Donec laoreet nonummy augue.",
+                    Price = 15000,
+                    PictureUrl = "/images/products/water-bottle.png",
+                    Brand = "Others",
+                    Type = "Accessories",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Toilet Litter Box",
+                    Description =
+                        "Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+                    Price = 18000,
+                    PictureUrl = "/images/products/toilet.png",
+                    Brand = "Others",
+                    Type = "Accessories",
+                    QuantityInStock = 100
+                },
+            };
+
+            foreach (var product in products)
+            {
+                context.Products.Add(product);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
