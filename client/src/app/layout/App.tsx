8dd3d0593419e7b8b916a02371e2c3e460a123ee
Yujia <0404sola@gmail.com>
2024-09-06 21:02:14 +1000

    Add catalog component

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+import { Product } from "../models/product";
+import Catalog from "../../features/catalog/Catalog";
+
 function App() {
+	const [products, setProducts] = useState<Product[]>([]);
+
+	useEffect(() => {
+		fetch("http://localhost:5000/api/products")
+			.then((response) => response.json())
+			.then((data) => setProducts(data));
+	}, []);
+
+	function addProduct() {
+		console.log("Adding product");
+	}
+
 	return (
 		<div>
 			<h1>One Stop Bunny Shop</h1>
+			<Catalog products={products} addProduct={addProduct} />
 		</div>
 	);
 }
